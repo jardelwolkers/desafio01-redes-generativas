4ec1b8b9eef20ea533b329ee06e447138ae2c1a9
Jardel Wolkers <jardelwolkers@gmail.com>
2023-07-12 23:15:12 -0300

    another commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css'; // Import the CSS file for styling
 
 // Login page component
@@ -28,22 +30,23 @@ const LoginPage = () => {
       <input type="text" placeholder="Nome" value={nome} onChange={handleNomeChange} required />
       <input type="password" placeholder="Senha" value={senha} onChange={handleSenhaChange} required />
       <button onClick={handleEntrarClick}>Entrar</button>
-      <Link to="/esqueci-minha-senha" className="forgot-password-link">Esqueci minha senha</Link>
+      <Link to="/esqueci-minha-senha" className="forgot-password-link">
+        Esqueci minha senha
+      </Link>
       {redirectToProducts && <Navigate to="/products" />}
     </div>
   );
 };
 
 // Product page component
-// Product page component
 const ProductPage = () => {
   const products = [
-    { image: 'image1.webp', description: 'ImgCreator.ai  batman car with sherek ', price: '$10' },
-    { image: 'image2.webp', description: 'ImgCreator.ai  bmw car in front of the 66 route', price: '$15' },
-    { image: 'image3.webp', description: 'ImgCreator.ai  ferrari car in front of the beach', price: '$20' },
-    { image: 'image4.webp', description: 'ImgCreator.ai  fusca car in front of the pao de acucar', price: '$30' },
-    { image: 'image5.webp', description: 'ImgCreator.ai  pink truck with woman driving a little bit angry', price: '$100' },
-    { image: 'image6.webp', description: 'ImgCreator.ai  manufacturing car industry', price: '$40' },
+    { image: 'image1.webp', description: 'ImgCreator.ai batman car with shrek', price: '$10' },
+    { image: 'image2.webp', description: 'ImgCreator.ai bmw car in front of the 66 route', price: '$15' },
+    { image: 'image3.webp', description: 'ImgCreator.ai ferrari car in front of the beach', price: '$20' },
+    { image: 'image4.webp', description: 'ImgCreator.ai fusca car in front of the pao de acucar', price: '$30' },
+    { image: 'image5.webp', description: 'ImgCreator.ai pink truck with woman driving a little bit angry', price: '$100' },
+    { image: 'image6.webp', description: 'ImgCreator.ai manufacturing car industry', price: '$40' },
     // Add more products with image, description, and price here
   ];
 
@@ -65,13 +68,33 @@ const ProductPage = () => {
   );
 };
 
-
+// Forgot password page component
 // Forgot password page component
 const ForgotPasswordPage = () => {
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate(); // Add this line
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleResetPassword = () => {
+    if (!email) {
+      toast.error('Please enter your email address');
+      return;
+    }
+
+    toast.success('O link para redefinição foi enviado para o endereço informado');
+
+    setEmail('');
+    navigate('/login'); // Redirect to login page
+  };
+
   return (
     <div className="container">
       <h2>Esqueci minha senha</h2>
-      {/* Forgot password form and other components */}
+      <input type="email" placeholder="E-mail" value={email} onChange={handleEmailChange} required />
+      <button onClick={handleResetPassword}>Redefinir a senha</button>
     </div>
   );
 };
@@ -81,12 +104,15 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/products" element={<ProductPage />} />
         <Route path="/esqueci-minha-senha" element={<ForgotPasswordPage />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
+      <ToastContainer />
     </Router>
   );
 };
+
 
 export default App;
